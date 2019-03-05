c0bbaa91d4eedef1b3e8f6b83d5628dd3357d3ee
timbauer <timbauer@ymail.com>
2019-03-05 11:31:41 -0800

    cleaned up docs for SearchEngine.py

--- SearchEngine.py
+++ SearchEngine.py
@@ -14,22 +14,24 @@ stemmer = PorterStemmer()
 print(type(invertedIndex))
 
 # write inverted index into file with easy to read format
-for item in invertedIndex:
-
-    termData = invertedIndex[item]
+for term in invertedIndex:
+    # this var gives us access to object that stores things like postings lists & term doc freq for given term
+    termData = invertedIndex[term]
+    # posting list for the current term
     postingList = termData.getPostingList()
-    print(item + " : " +  str(termData.getDocumentFrequency()))
-    indexFile.write((item + " : " +  str(termData.getDocumentFrequency())) + "\r")
 
+    # prints/writes term name and df_t
+    print(term + " : " +  str(termData.getDocumentFrequency()))
+    indexFile.write((term + " : " +  str(termData.getDocumentFrequency())) + "\r")
+
+    # for loop gets each docId associated with term and prints out term positions & frequencies
     for docId in postingList:
+        # prints/writes docId & freq of term in that specific doc
         print("\t" + docId + ", " + str(postingList[docId].getTermDocumentFrequency()) + " :", end =" ")
         indexFile.write("\t" + docId + ", " + str(postingList[docId].getTermDocumentFrequency()) + " : ")
+        # prints out a list of each numerical position of the term in the doc
         for wordPosition in postingList[docId].getTermPositionList():
             print(" " + str(wordPosition) + ",", end ="")
             indexFile.write(" " + str(wordPosition) + ",")
         print("\r")
         indexFile.write("\r")
-
-
-    # indexFile.write("\r\t" + str(invertedIndex[item].getFrequency()))
-    # indexFile.write("\r\t" + str(invertedIndex[item].getPostingList()) + "\r\r")
